7c8dad591c8d45e917b0d9a17cd398c82df6eac4
Purwanto <purwanto@pusilkom.ui.ac.id>
2011-03-17 10:45:04 +0700

    - perbaikan pada pembayaran transaksi, cross check apakah data sudah benar2   tersimpan di table item_transaksi_penjualan, jika ternyata belum maka pembayaran dibatalkan.

--- system/application/models/item_transaksi.php
+++ system/application/models/item_transaksi.php
@@ -26,6 +26,20 @@ class Item_transaksi extends Model
         $query = 'select id_transaksi,id_barang, sum(qty) as qty,diskon from item_transaksi_penjualan where id_transaksi="'.$id_transaksi.'" group by id_barang, diskon';
         return $this->db->query($query);
     }
+    /**
+    * Ambil item transaksi per id transaksi tanpa diakumulasi
+    */
+    function get_all_item($id_transaksi) 
+    {
+        return $this->db->get_where('item_transaksi_penjualan',array('id_transaksi'=>$id_transaksi));
+    }
+    /**
+    * Hapus id transaksi tertentu
+    */
+    function remove($id_transaksi)
+    {
+        return $this->db->where('id_transaksi',$id_transaksi)->delete('item_transaksi_penjualan');
+    }
 }
 //End of item_transaksi.php
 //Location: system/application/models
