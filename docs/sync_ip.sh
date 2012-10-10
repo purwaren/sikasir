5136c7ab570b14f5f03866a64a6b9c1441c870c0
Purwa@Purwa-PC <Purwa@Purwa-PC>
2012-10-10 13:39:47 +0700

    update sync ip, change server address

--- docs/sync_ip.sh
+++ docs/sync_ip.sh
@@ -1,7 +1,6 @@
 echo "Sync IP every 300 seconds"
 while true
 do
-curl -u "perbaungan:PhaTh4fuTUste6ar4gePacHu2uzacRAj" -d "store_code=17&name=Mode Fashion Perbaungan" -X POST http://152.118.31.168/yii/app/dashboard/index.php/syncronizeKasir/syncIp > output.txt
-echo "\n" > output.txt
+curl -u "perbaungan:PhaTh4fuTUste6ar4gePacHu2uzacRAj" -d "store_code=17&name=Mode Fashion Perbaungan" -X POST http://dashboard.modefashiongroup.com/index.php/syncronizeKasir/syncIp
 sleep 300
 done
