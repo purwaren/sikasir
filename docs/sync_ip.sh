1ec88a246bd603a0b4992a1fa63cea61df01e3b7
Purwa@Purwa-PC <Purwa@Purwa-PC>
2012-10-10 15:42:02 +0700

    update sync tambah user agent

--- docs/sync_ip.sh
+++ docs/sync_ip.sh
@@ -1,6 +1,6 @@
 echo "Sync IP every 300 seconds"
 while true
 do
-curl -u "perbaungan:PhaTh4fuTUste6ar4gePacHu2uzacRAj" -d "store_code=17&name=Mode Fashion Perbaungan" -X POST http://dashboard.modefashiongroup.com/index.php/syncronizeKasir/syncIp
+curl -A "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.79 Safari/537.4" -u "perbaungan:PhaTh4fuTUste6ar4gePacHu2uzacRAj" -d "store_code=17&name=Mode Fashion Perbaungan" -X POST http://dashboard.modefashiongroup.com/index.php/syncronizeKasir/syncIp
 sleep 300
 done
