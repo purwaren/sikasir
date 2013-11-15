3d348af63a68202dd24647dc4d0f3420386dfd35
Purwa@Purwa-PC <Purwa@Purwa-PC>
2013-11-15 13:44:05 +0700

    Pindahin data-data backup dari folder docs..

--- docs/TestSocket.java
+++
@@ -1,8 +0,0 @@
-public class TestSocket {
-	public static void main(String args) {
-		Socket printer = new Socket("192.168.1.112",20000);
-		BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(printer.getOutputStream()));
-		wr.write("aString#");
-		wr.flush();
-	}
-}
